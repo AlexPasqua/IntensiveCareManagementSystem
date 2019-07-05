83eca66a3895d9469bd69a2e911b52c61c451d9f
AlexPasqua <alexpasquali98@gmail.com>
2019-07-05 17:54:18 +0200

    'Doctor' updated

--- src/sample/Doctor.java
+++ src/sample/Doctor.java
@@ -5,4 +5,12 @@ public class Doctor extends Nurse {
     public Doctor(String name, String surname, String username, String password){
         super(name, surname, username, password);
     }
+
+    public void setDiagnosis(Patient pat, String diagnosis){
+        pat.setDiagnosis(diagnosis);
+    }
+
+    public void addPrescription(Patient pat, String medicine, Integer therapyDuration, Integer dailyDoses, Integer mgDose){
+        pat.addPrescription(new Prescription(medicine, therapyDuration, dailyDoses, mgDose, this));
+    }
 }
