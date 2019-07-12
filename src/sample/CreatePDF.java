35eacf5f85eecada18b6d63908110b7152318e25
Francesco Fattori <ffattori@me.com>
2019-07-12 14:09:53 +0200

    -CreatePDF: sistemato Header PDF

--- src/sample/CreatePDF.java
+++ src/sample/CreatePDF.java
@@ -2,10 +2,12 @@ package sample;
 
 
 import com.itextpdf.forms.PdfAcroForm;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
@@ -18,34 +20,29 @@ class CreatePDF{
         Patient p1 = Datastore.getPatients().get(0);
 
         //pdf
-        String file = "N:/test.pdf";
+        String file = "C:\\Users\\Franc\\Downloads\\test\\test.pdf";
         PdfDocument pdfDoc = new PdfDocument(new PdfReader(CreatePDF.class.getResourceAsStream("/template.pdf")),new PdfWriter(file));
-        PdfAcroForm form = PdfAcroForm.getAcroForm(pdfDoc, true);
         Document doc = new Document(pdfDoc);
 
 
         //spazio header
-        doc.add(new Paragraph("\n\n\n\n\n\n\n\n\n\n\n\n"));
+        doc.add(new Paragraph("\n\n\n"));
+        Table firstTab = new Table(2).useAllAvailableWidth();
+        addHeader(firstTab, pdfDoc, p1, "01/01/2019-07/01/2019");
+        doc.add(firstTab);
 
-        //Name
-        Paragraph p = new Paragraph("Francesco Fattori");
-        p.setFixedPosition(32, 688, 240);
-        doc.add(p);
-        //Date
-        p = new Paragraph("01/01/2019-07/01/2019");
-        p.setFixedPosition(270, 688, 240);
-        doc.add(p);
-        //diagnosis
-        p = new Paragraph("Tutto bene dai");
-        System.out.println(p.getHeight());
-        p.setFixedPosition(32, 615, 240);
-        doc.add(p);
+        Paragraph titleDiagnosis = new Paragraph("Diagnosi Iniziale:\n").setBold();
+        Paragraph diagnosis = new Paragraph(p1.getDiagnosis());
+        doc.add(titleDiagnosis);
+        doc.add(diagnosis);
+        doc.add(new Paragraph("\n\n\n\n\n\n\n"));
+
 
         //Creating a table
         Table table = new Table(2).useAllAvailableWidth();
         //Adding cells to the table
-        table.addCell(new Cell().add(new Paragraph("Data")));
-        table.addCell(new Cell().add(new Paragraph("Data")));
+
+
 
         //Adding Table to document
         doc.add(table);
@@ -53,5 +50,19 @@ class CreatePDF{
         //Closing the document
         doc.close();
         System.out.println("Table created successfully..");
+
+    }
+
+    private static void addHeader(Table table, PdfDocument pdfDoc, Patient p, String date ){
+        table.addCell(new Cell().add(new Paragraph("Paziente").setBold()).setBorder(Border.NO_BORDER));
+        table.addCell(new Cell().add(new Paragraph("Intervallo Date").setBold()).setBorder(Border.NO_BORDER));
+        Cell cell = new Cell().add(new Paragraph(p.getFullName() + "\n" + p.getCodFis() + "\n" + p.getBirthTown() + "\n"));
+        cell.setBorder(Border.NO_BORDER);
+        cell.setMaxWidth((pdfDoc.getPage(1).getPageSizeWithRotation().getWidth()/2)-30);
+        table.addCell(cell);
+        Cell cell2 = new Cell().add(new Paragraph(date));
+        cell2.setBorder(Border.NO_BORDER);
+        cell2.setMaxWidth((pdfDoc.getPage(1).getPageSizeWithRotation().getWidth()/2)-30);
+        table.addCell(cell2);
     }
 }
